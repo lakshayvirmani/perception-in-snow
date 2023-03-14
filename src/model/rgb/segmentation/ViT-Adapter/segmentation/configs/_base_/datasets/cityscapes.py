f065386b7399aa34c79e02ef7dc3a28133e83b20
edit <lvirmani@andrew.cmu.edu>
2023-03-14 21:25:41 +0000

    Cityscapes augreg adapter tiny training running.

--- src/model/rgb/segmentation/ViT-Adapter/segmentation/configs/_base_/datasets/cityscapes.py
+++ src/model/rgb/segmentation/ViT-Adapter/segmentation/configs/_base_/datasets/cityscapes.py
@@ -3,7 +3,7 @@ dataset_type = 'CityscapesDataset'
 data_root = 'data/cityscapes/'
 img_norm_cfg = dict(
     mean=[123.675, 116.28, 103.53], std=[58.395, 57.12, 57.375], to_rgb=True)
-crop_size = (512, 1024)
+crop_size = (512, 512)
 train_pipeline = [
     dict(type='LoadImageFromFile'),
     dict(type='LoadAnnotations'),
