058d569d55de61c884bb61952bb729f0f3f6694b
Lakshay Virmani <lvirmani@andrew.cmu.edu>
2023-03-16 19:52:38 -0400

    Preparing cadcdCustomDataset for mmseg library.

--- src/model/rgb/segmentation/ViT-Adapter/segmentation/configs/_base_/datasets/cadcdCustomDataset.py
+++ src/model/rgb/segmentation/ViT-Adapter/segmentation/configs/_base_/datasets/cadcdCustomDataset.py
@@ -5,8 +5,69 @@ import numpy as np
 from mmcv.utils import print_log
 from PIL import Image
 
-from mmseg.datasets.builder import DATASETS
-from mmseg.datasets.custom import CustomDataset
+from .builder import DATASETS
+from .custom import CustomDataset
+
+from collections import namedtuple
+
+
+#--------------------------------------------------------------------------------
+# Definitions
+#--------------------------------------------------------------------------------
+
+# a label and all meta information
+Label = namedtuple( 'Label' , [
+
+    'name'        , # The identifier of this label, e.g. 'car', 'person', ... .
+                    # We use them to uniquely name a class
+
+    'id'          , # An integer ID that is associated with this label.
+                    # The IDs are used to represent the label in ground truth images
+                    # An ID of -1 means that this label does not have an ID and thus
+                    # is ignored when creating ground truth images (e.g. license plate).
+                    # Do not modify these IDs, since exactly these IDs are expected by the
+                    # evaluation server.
+
+    'trainId'     , # Feel free to modify these IDs as suitable for your method. Then create
+                    # ground truth images with train IDs, using the tools provided in the
+                    # 'preparation' folder. However, make sure to validate or submit results
+                    # to our evaluation server using the regular IDs above!
+                    # For trainIds, multiple labels might have the same ID. Then, these labels
+                    # are mapped to the same class in the ground truth images. For the inverse
+                    # mapping, we use the label that is defined first in the list below.
+                    # For example, mapping all void-type classes to the same ID in training,
+                    # might make sense for some approaches.
+                    # Max value is 255!
+
+    'category'    , # The name of the category that this label belongs to
+
+    'categoryId'  , # The ID of this category. Used to create ground truth images
+                    # on category level.
+
+    'hasInstances', # Whether this label distinguishes between single instances or not
+
+    'ignoreInEval', # Whether pixels having this class as ground truth label are ignored
+                    # during evaluations or not
+
+    'color'       , # The color of this label
+    ] )
+
+
+#--------------------------------------------------------------------------------
+# A list of all labels
+#--------------------------------------------------------------------------------
+
+# Please adapt the train IDs as appropriate for your approach.
+# Note that you might want to ignore labels with ID 255 during training.
+# Further note that the current train IDs are only a suggestion. You can use whatever you like.
+# Make sure to provide your results using the original IDs and not the training IDs.
+# Note that many IDs are ignored in evaluation and thus you never need to predict these!
+
+CSLabels = [
+    #       name                     id    trainId   category            catId     hasInstances   ignoreInEval   color
+    Label(  'unlabeled'            ,  0 ,      255 , 'void'            , 0       , False        , True         , (  0,  0,  0) ),
+    Label(  'road'                 ,  7 ,        0 , 'flat'            , 1       , False        , False        , (128, 64,128) ),
+]
 
 @DATASETS.register_module()
 class CADCDataset(CustomDataset):
@@ -31,7 +92,6 @@ class CADCDataset(CustomDataset):
         """Convert trainId to id for CADC."""
         if isinstance(result, str):
             result = np.load(result)
-        import tools.cadcd.cadcdscripts.helpers.labels as CSLabels
         result_copy = result.copy()
         for trainId, label in CSLabels.trainId2label.items():
             result_copy[result == trainId] = label.id
@@ -69,7 +129,6 @@ class CADCDataset(CustomDataset):
             png_filename = osp.join(imgfile_prefix, f'{basename}.png')
 
             output = Image.fromarray(result.astype(np.uint8)).convert('P')
-            import tools.cadcd.cadcdscripts.helpers.labels as CSLabels
             palette = np.zeros((len(CSLabels.id2label), 3), dtype=np.uint8)
             for label_id, label in CSLabels.id2label.items():
                 palette[label_id] = label.color
@@ -118,7 +177,7 @@ class CADCDataset(CustomDataset):
                  metric='mIoU',
                  logger=None,
                  imgfile_prefix=None):
-        """Evaluation in Cityscapes/default protocol.
+        """Evaluation in CADC/default protocol.
         Args:
             results (list): Testing results of the dataset.
             metric (str | list[str]): Metrics to be evaluated.
@@ -134,15 +193,11 @@ class CADCDataset(CustomDataset):
                 will be created for evaluation.
                 Default: None.
         Returns:
-            dict[str, float]: Cityscapes/default metrics.
+            dict[str, float]: CADC/default metrics.
         """
 
         eval_results = dict()
         metrics = metric.copy() if isinstance(metric, list) else [metric]
-        if 'cityscapes' in metrics:
-            eval_results.update(
-                self._evaluate_cityscapes(results, logger, imgfile_prefix))
-            metrics.remove('cityscapes')
         if len(metrics) > 0:
             eval_results.update(
                 super(CADCDataset,
